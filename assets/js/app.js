711072618595e5aaadd9adc4eeaba444cccbc46a
vanehuanchicay <vanesapardoh@gmail.com>
2018-02-08 09:55:06 -0300

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
  
 function initMap() {
   var map = new google.maps.Map(document.getElementById('mapa'),{
-    zoom: 18,
+    zoom: 15,
     center: {lat: -12.1191427, lng: -77.0349046},
     mapTypeControl: false,
     streetViewControl: false
@@ -11,28 +11,28 @@ function initMap() {
       navigator.geolocation.getCurrentPosition(success, error);
     }
   }
+
+  var bike = {
+    url:'assets/img/bike.svg',
+    scaledSize: new google.maps.Size(50, 50),
+    anchor: new google.maps.Point(5,10)
+  };
  // document.getElementById('encuentrame').addEventListener('click', search);
+  
   var latitud, longitud;
 
   var success = function(position){
     latitud = position.coords.latitude;
     longitud = position.coords.longitude;
 
-  var bike ={
-    url:'assets/img/bike.svg',
-    scaledSize: new google.maps.Size(50, 50),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(0, 0)
-  };
-
+  
   var miUbicacion = new google.maps.Marker({
     position : {lat: latitud, lng: longitud},
-    animation: google.maps.Animation.DROP,
+    animation: google.maps.Animation.BOUNCE,
     map: map,
     icon: bike
   });
-
-  map.setZoom(18);
+  map.setZoom(15);
   map.setCenter({lat: latitud, lng: longitud});
 
 }
@@ -47,14 +47,22 @@ function initMap() {
   new google.maps.places.Autocomplete(inputFinish);
 
   var directionsService = new google.maps.DirectionsService();
-  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
+
    directionsDisplay.setMap(map);
      var trazarRuta = function(){
      calculateAndDisplayRoute(directionsService, directionsDisplay);
   };
   document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
+  function makeMarker(position, icon, title){
+    new google.maps.Marker({
+      position: position,
+      map: map,
+      icon: bike,
+      title:title
+    });
 
-
+  }
   var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
       directionsService.route({
         origin: inputStart.value,
@@ -63,6 +71,9 @@ function initMap() {
       },function(response, status){
       if(status === 'OK'){
         directionsDisplay.setDirections(response);
+        var leg = response.routes[ 0 ].legs[ 0 ];
+        makeMarker(leg.start_location, bike, 'Origen');
+        makeMarker(leg.end_location, bike, 'Destino');
       }else{
         window.alert('No encontramos una ruta.');
       }
@@ -70,69 +81,3 @@ function initMap() {
   }
   search();
 }
-/*
-
-  function initMap() {
-
-
-  function search(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-
-  }
- 
-  var mapa = document.getElementById('mapa');
-  navigator.geolocation.getCurrentPosition(success, error);
-  function error(){};
-
-  function success(position){
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  new google.maps.LatLng(lat, long);
-
-  var place = new google.maps.LatLng(lat, long);
-  var objConfig = {
-    zoom: 17,
-    center: place
-  };
-
-  var gMap = new google.maps.Map(mapa, objConfig);
-  var objConfigMarker = {
-    position: place,
-    map : gMap
-    }
-    var gMarker = new google.maps.Marker(objConfigMarker);
-  }
-  var inputStart = document.getElementById('start');
-  var inputFinish = document.getElementById('finish');
-
-  new google.maps.places.Autocomplete(inputStart);
-  new google.maps.places.Autocomplete(inputFinish);
-
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-
-
-  var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
-      directionsService.route({
-        origin: inputStart.value,
-        destination: inputFinish.value,
-        travelMode: 'DRIVING'
-      },function(response, status){
-      if(status === 'OK'){
-        directionsDisplay.setDirections(response);
-      }else{
-        window.alert('No encontramos una ruta.');
-      }
-    });
-
-     directionsDisplay.setMap(mapa);
-     var trazarRuta = function(){
-     calculateAndDisplayRoute(directionsService, directionDisplay);
-  };
-
-  document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
-  }
-}
-*/
